3477a68fb96dc4cde7d8ab976762bdd73f8cba53
abrilxcx <abrlbjrn@gmail.com>
2024-07-31 12:42:52 -0600

    Updated CompletedPage structure to new one
    From the previous setup of list of done and undone tasks

--- src/pages/CompletedPage.jsx
+++ src/pages/CompletedPage.jsx
@@ -1,18 +1,13 @@
-import { useContext } from "react";
 import { DeleteCompletedBtn } from "../components/DeleteCompletedBtn";
 import { TaskList } from "../components/TaskList";
-import { TaskContext } from "../context/TaskContext";
 
 
 export const CompletedPage = () => {
-
-   const { tasks } = useContext( TaskContext );
-
-
+   
    return (
       <>
          <DeleteCompletedBtn />
-         <TaskList tasks={ tasks } />
+         <TaskList/>
       </>
    )
 }
